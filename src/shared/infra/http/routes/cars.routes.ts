5a00ae8863e7e9c2c406989b984b4d199bded692
Franciel Santos <francielvendas@outlook.com>
2021-05-31 17:09:44 -0300

    administrator authentication

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,9 +2,17 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/car/useCases/createCar/CreateCarController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticate";
+
 const carRoutes = Router();
 const createCarController = new CreateCarController();
 
-carRoutes.post("/", createCarController.handle);
+carRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carRoutes };
